e13f2b4e45b3e99e832d31021a9ce2684e15362b
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2023-09-25 16:19:07 +0530

    Moved to dev-develop atk

--- src/LaravelAtkApp.php
+++ src/LaravelAtkApp.php
@@ -2,15 +2,22 @@
 
 namespace Gowrav\LaravelAtkIntegrate;
 
-use Atk4\Ui\Exception\LateOutputError;
-use Atk4\Ui\Exception\UnhandledCallbackExceptionError;
-use Throwable;
 
 class LaravelAtkApp extends \Atk4\Ui\App {
-    protected $url_building_ext = '';
+    protected string $urlBuildingExt = '';
+
+    /** @var array|false Location where to load JS/CSS files */
+    public $cdn = [
+        'atk' => 'atk',
+        'jquery' => 'atk/external/jquery/dist',
+        'fomantic-ui' => 'atk/external/fomantic-ui/dist',
+        'flatpickr' => 'atk/external/flatpickr/dist',
+        'highlight.js' => 'atk/external/@highlightjs/cdn-assets',
+        'chart.js' => 'atk/external/chart.js/dist', // for atk4/chart
+    ];
     
-    public function initIncludes() {
-        $this->html->js(true,new \Atk4\Ui\JsExpression(
+    public function initIncludes(): void {
+        $this->html->js(true,new \Atk4\Ui\Js\JsExpression(
             '$.ajaxSetup({
                 headers: {
                     \'X-CSRF-TOKEN\': \'' . csrf_token() . '\'
